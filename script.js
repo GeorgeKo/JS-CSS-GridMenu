00e8f93cc661053fb60213ee3e51b0bab19f6b66
GeorgeKo <hello@giko.gr>
2020-04-08 06:24:18 +0300

    js display:none, better classification

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 $(".button.menu").click(function(){
     $(".container").toggleClass("hidden");
+    $(".container").toggleClass("show");
+    if ( $(".container").hasClass("hidden")){
+        setTimeout(function(){
+            $(".menu").hide();
+        }, 1800);
+    }else{
+        $(".button:not(.menu)").show();
+    }
 });
+
